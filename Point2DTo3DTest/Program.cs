fa78283005e345820d6513aed11f85405b5ee03b
Piero Castillo <pierocastillo2005@gmail.com>
2021-08-25 10:22:37 -0500

    Agregar archivos de proyecto.

---
+++ Point2DTo3DTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Point2DTo3DTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 700;
+            int y = 500;
+            int pWidth = 800;
+            int pHeight = 600;
+
+            float npX = ((float)x / pWidth * 2) - 1;
+            float npY = ((float)y / pHeight * 2) - 1;
+
+            Console.WriteLine($"x : {x}");
+            Console.WriteLine($"y : {y}");
+            Console.WriteLine($"width : {pWidth}");
+            Console.WriteLine($"height : {pHeight}");
+
+            Console.WriteLine($"screeenCoordX : {npX}");
+            Console.WriteLine($"screeenCoordY : {npY}");
+        }
+    }
+}
